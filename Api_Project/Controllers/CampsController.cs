366bac4a9fd6c22ca7d0cabd616247a9b058018e
ilhambinyetov <binyetov631@gmail.com>
2023-01-30 01:12:19 +0400

    Create Camps Controller

---
+++ Api_Project/Controllers/CampsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Project.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {   
+      
+        public object Get()
+        {
+            return new { Name = "Ilham", Title = "Book"};
+        }
+    }
+}
